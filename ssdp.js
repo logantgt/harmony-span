bd6c5b982bede6346bd82d6c37557ea5bf6ef435
Logan Lowe <loganr.lowe@gmail.com>
2020-07-05 14:07:37 -0600

    SSDP server now runs on localhost
    Let's make things even easier! SSDP will just broadcast on localhost now.

--- ssdp.js
+++ ssdp.js
@@ -7,25 +7,17 @@ module.exports.run = async () =>
     var ip = JSON.parse(fs.readFileSync('config.json', 'utf8')).ip;
     ssdp = new Server
     ({
-        "location":"http://" + ip + ":8060/",
+        "location":"http://localhost:8060/",
         "udn":"uuid:roku:ecp:HARMONYSPAN",
         "ssdpSig":'Server: Roku/9.3.0 UPnP/1.0 Roku/9.3.0'
     });
-
-    if(ip == "xxx.xxx.xxx.xxx")
-    {
-        colorout.Log("error", "Please set your machine's local IPv4 address in config.json! Cannot start SSDP server.");
-        process.exit();
-    }
-    else
-    {
-        colorout.Log("success", "SSDP server running at " + ip + "!");
-    }
     
     ssdp.addUSN("roku:ecp");
     
     ssdp.start();
     
+    colorout.Log("success", "SSDP server running on localhost!");
+
     process.on('exit', function()
     {
         ssdp.stop();
